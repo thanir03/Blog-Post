75e873e098687e967b9dfda3fa73e4908b86c6a1
thanir03 <2003thanir10@gmail.com>
2022-11-30 14:43:24 +0800

    Lazy-loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import BlogPage from "./pages/BlogPage/BlogPage";
-import NewBlogPage from "./pages/NewBlogPage/NewBlogPage";
-import BlogDetailsPage from "./pages/BlogDetailsPage/BlogDetailsPage";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
 import Navigation from "./components/Navigation/Navigation";
+import Loading from "./components/UI/Loading/Loading";
+
+const BlogDetailsPage = lazy(() =>
+  import("./pages/BlogDetailsPage/BlogDetailsPage")
+);
+const BlogPage = lazy(() => import("./pages/BlogPage/BlogPage"));
+const NewBlogPage = lazy(() => import("./pages/NewBlogPage/NewBlogPage"));
 
 function App() {
   return (
     <>
       <Navigation>
-        <Routes>
-          <Route index element={<WelcomePage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:blogId" element={<BlogDetailsPage />} />
-          <Route path="/blog/new" element={<NewBlogPage />} />
-          <Route path="/error" element={<h1>404</h1>} />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="loading">
+              <Loading />
+            </div>
+          }
+        >
+          <Routes>
+            <Route index element={<WelcomePage />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog/:blogId" element={<BlogDetailsPage />} />
+            <Route path="/blog/new" element={<NewBlogPage />} />
+            <Route path="/error" element={<h1>404</h1>} />
+          </Routes>
+        </Suspense>
       </Navigation>
     </>
   );
 }
 
 export default App;
+
+// server need to ignore the url parameter and just send the index.html or / resources 
